12cb1a9527b867d83b8fa5873495f0727e246722
hanabbi <megamihanasan@gmail.com>
2023-10-30 15:26:42 +0900

    version 1.9.1

--- Source/Atmosphere/stratosphere/loader/source/oc/customize.hpp
+++ Source/Atmosphere/stratosphere/loader/source/oc/customize.hpp
@@ -16,7 +16,7 @@
 
 #pragma once
 
-#define CUST_REV 10
+#define CUST_REV 11
 
 #include "oc_common.hpp"
 #include "pcv/pcv_common.hpp"
--- pages/src/main.ts
+++ pages/src/main.ts
@@ -1,5 +1,5 @@
 /* Config: Cust */
-const CUST_REV_ADV = 10;
+const CUST_REV_ADV = 11;
 
 enum CustPlatform {
   Undefined = 0,
